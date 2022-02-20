2330b610c23acb355d2acd80b64ed8f93de111b8
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 21:24:50 +0300

    test class to define the test cases created

---
+++ user_credentials_test.py
@@ -0,0 +1,8 @@
+from re import U
+import unittest
+from user_credentials import Usercredentials
+
+class TestUsercredentials(unittest.TestCase):
+    '''
+    test class that define test cases for the usercredentials behaviours
+    '''
