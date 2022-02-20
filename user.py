71d4731053e8b41483b8392136cfd19a5482dca7
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 21:20:16 +0300

    user class created and init method defined

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    '''
+    class to generate new instances of users
+    '''
+    user_list = [] #empty list to append users
+
+    def __init__(self,first_name,last_name,email,user_name,password):
+        '''
+        method to define the properties of the object
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.user_name = user_name
+        self.password = password
