71f90cd6c77eadd3ea4c60dd31ee84a00a0c586d
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 21:57:08 +0300

    setup method and test case tp check proper initialization created

--- user_test.py
+++ user_test.py
@@ -6,3 +6,21 @@ class TestUser(unittest.TestCase):
     '''
     test class that defines test cases for the User class behaviors
     '''
+
+    def setUp(self):
+
+        self.new_user = User("michael","muga","michael@gmail.com","mike","febr2000")
+
+    def test__init(self):
+        '''
+        test case to test if the obect is initialised properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"michael")
+        self.assertEqual(self.new_user.last_name,"muga")
+        self.assertEqual(self.new_user.email,"michael@gmail.com")
+        self.assertEqual(self.new_user.user_name,"mike")
+        self.assertEqual(self.new_user.password,"febr2000")
+
+if __name__ == '__main__':
+    unittest.main()
