600d4a17eb3d748b0de75dc0d9f3e4ffb156f2b7
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 23:29:31 +0300

    test method to save multiple users added

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -16,10 +16,22 @@ class TestUsercredentials(unittest.TestCase):
         self.assertEqual(self.new_credentials.site_name,"gmail")
         self.assertEqual(self.new_credentials.password,"254mike")
 
+    def tearDown(self):
+        '''
+        teardown method that clean up after each test case has run
+        '''   
+        Usercredentials.user_credential_list=[]
+
     def test_save_credential(self):
 
         self.new_credentials.save_credentials()
         self.assertEqual(len(Usercredentials.user_credential_list),1)
+
+    def test_save_multiple_credentials(self):
+        self.new_credentials.save_credentials()
+        test_credential = Usercredentials("twitter","787627")
+        test_credential.save_credentials()
+        self.assertEqual(len(Usercredentials.user_credential_list),2)
 
 if __name__ == '__main__':
     unittest.main()    
