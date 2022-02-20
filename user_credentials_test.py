bf5689f450a62e6885f740c91133db8674f824c3
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 22:20:51 +0300

    setup test init and test to save credentials method created

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -6,3 +6,20 @@ class TestUsercredentials(unittest.TestCase):
     '''
     test class that define test cases for the usercredentials behaviours
     '''
+
+    def setUp(self):
+
+        self.new_credentials = Usercredentials("gmail","254mike")
+
+    def test__init(self):
+
+        self.assertEqual(self.new_credentials.site_name,"gmail")
+        self.assertEqual(self.new_credentials.password,"254mike")
+
+    def test_save_credential(self):
+
+        self.new_credentials.save_credentials()
+        self.assertEqual(len(Usercredentials.user_credential_list),1)
+
+if __name__ == '__main__':
+    unittest.main()    
