ae25913bdf0fc83335270896bbd672a09cef26d1
michael-muga <mugamichaelshawn@gmail.com>
2022-02-20 21:23:22 +0300

    class usercredentials and method to define the properties of the object created

---
+++ user_credentials.py
@@ -0,0 +1,14 @@
+class Usercredentials:
+    '''
+    class to generate new instances of usercredentials
+    '''
+
+    user_credential_list = [] #empty list for user creddential
+
+    def __init__(self,site_name,password):
+        '''
+        method to define properties of the object
+        '''
+
+        self.site_name = site_name
+        self.password = password
